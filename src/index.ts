a6ca286a2bf503262d8439ef9a55c1ff37047f5f
Fabian Vilers <fabian.vilers@dev-one.com>
2022-12-31 12:53:02 +0100

    Add the disposable interface and using function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+export interface Disposable {
+  dispose(): void;
+}
+
+type ResourceBlockCallback<T extends Disposable> = (resource: T) => void;
+
+export function using<T extends Disposable>(
+  resource: T,
+  callback: ResourceBlockCallback<T>
+) {
+  try {
+    callback(resource);
+  } finally {
+    resource.dispose();
+  }
+}
